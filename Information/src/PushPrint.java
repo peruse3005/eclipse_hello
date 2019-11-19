3bbb8d1b1f0d901cd7dc546819b6884aa2a9c9f9
peruse3005 <43398485+peruse3005@users.noreply.github.com>
2019-11-20 02:47:54 +0900

    1

--- Information/src/PushPrint.java
+++ Information/src/PushPrint.java
@@ -3,11 +3,8 @@ public class PushPrint {
 
 	public static void main(String[] args) {
 		System.out.println("Add Java Class File Push");
-<<<<<<< HEAD
-		System.out.println("World with Miku");
-=======
 		System.out.println("World With 42");
->>>>>>> refs/remotes/origin/master
+		System.out.println("World with Miku");
 	}
 
 }

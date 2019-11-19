4853395bfc4b7ef71427a7b372469c0df6673666
Jaehoon Choi <peruse3005@naver.com>
2019-11-20 00:49:42 +0900

    ComputerSW

--- Information/src/Information.java
+++ Information/src/Information.java
@@ -4,6 +4,7 @@ public class Information {
 	public static void main(String[] args) {
 		System.out.println("√÷¿Á»∆");
 		System.out.println("20183287");
+		System.out.println("ComputerSW");
 	}
 
 }
